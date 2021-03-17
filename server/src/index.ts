5ab93b14680c18837d7a2d87b91e7ac1bd9ff7db
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-17 14:21:50 +0100

    db connected

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -9,4 +14,6 @@ app.use('/api/buckets', require('./routes/buckets'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
