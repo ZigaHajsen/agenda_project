998ff274179d7afe4afb22be37a906c6e44b8164
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-19 12:42:09 +0100

    refactored buckets page into app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { BucketList } from './pages';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { Buckets, Files } from './components';
+import { Navbar } from 'react-bootstrap';
 
 const App = () => {
+  const render = useSelector((state: any) => state.render.files);
+
   return (
-    <div>
-      <BucketList />
-    </div>
+    <Fragment>
+      <Navbar bg='dark'>
+        <Navbar.Brand>Secure cloud storage</Navbar.Brand>
+      </Navbar>
+      {render ? <Files /> : <Buckets />}
+    </Fragment>
   );
 };
 
