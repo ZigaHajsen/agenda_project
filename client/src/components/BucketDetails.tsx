54c69118044943a2fc7b87fb3b5408e3d02fc85f
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-03-20 21:48:27 +0100

    added delete bucket modal

--- client/src/components/BucketDetails.tsx
+++ client/src/components/BucketDetails.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {
   filesOff,
@@ -5,15 +6,27 @@ import {
   bucketDetailsOff,
 } from '../redux/render/actions';
 import styled from 'styled-components/macro';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Row, Modal } from 'react-bootstrap';
 
 const BucketDetails = () => {
   const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
+  const openModal = () => {
+    setOpen(true);
+  };
+  const closeModal = () => {
+    setOpen(false);
+  };
+
+  const handleBackClick = () => {
     dispatch(removeBucket());
     dispatch(filesOff());
   };
+  const handleClick = () => {
+    console.log('click');
+  };
+
   return (
     <Container>
       <Row>
@@ -27,20 +40,30 @@ const BucketDetails = () => {
       </Row>
       <Nav className='mb-3 mt-3'>
         <div></div>
-        <Button variant='danger'>Delete Bucket</Button>
+        <Button variant='danger' onClick={openModal}>
+          Delete Bucket
+        </Button>
       </Nav>
-      <TableNav>
-        <Col>Name</Col>
-        <Col>Last Modified</Col>
-        <Col>Size</Col>
-      </TableNav>
-      <Border></Border>
+      <Row>Bucket name:</Row>
+      <Row>Location:</Row>
+      <Row>Storage size:</Row>
       <Nav>
         <div></div>
-        <Button variant='danger' className='mt-3' onClick={handleClick}>
+        <Button variant='danger' className='mt-3' onClick={handleBackClick}>
           Back
         </Button>
       </Nav>
+      <Modal show={open} onHide={closeModal}>
+        <Modal.Body>Do you really want to delete this bucket?</Modal.Body>
+        <Modal.Footer>
+          <Button variant='success' onClick={closeModal}>
+            Close
+          </Button>
+          <Button variant='danger' type='submit' onClick={handleClick}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
@@ -50,13 +73,4 @@ export default BucketDetails;
 const Nav = styled(Row)`
   display: flex;
   justify-content: space-between;
-`;
-
-const TableNav = styled(Row)`
-  border: black 2px solid;
-  background: grey;
-`;
-
-const Border = styled(Row)`
-  border-bottom: black 2px solid;
 `;
